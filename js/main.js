4ac9bd7df2478c23ebe26a50a286bd7a90e9979f
programicoTeam <programico@outlook.com>
2020-04-11 21:04:32 +0200

    client input updated

--- js/main.js
+++ js/main.js
@@ -2,10 +2,40 @@ var clickedElementText;
 var clickedElementId;
 var numberOfClick = 0;
 var selectedYears = document.getElementsByClassName("yearPanel");
-var howManyYears = 3;
+var howManyYears;
+var yearSelected;
+var currentDate = new Date();
+var currentYear = currentDate.getFullYear();
+
+
+
+window.onload = function(){
+   
+
+   // page fully loaded
+
+
+   
+
+
+
+};
+
+function getValue(){
+    yearSelected = document.getElementById('begYear').value;
+    
+    howManyYears =  (currentYear - yearSelected) - 1;
+
+    // create new tabs and panels that are equal to the value on howManyYears variable
+
+    createTabsNPanels(yearSelected);
+}
+
 
 document.getElementById("list").addEventListener("click", function(e) {
     
+    
+
     // get the text inside the clicked item
     clickedElementText = e.target.innerText;
     
@@ -16,23 +46,19 @@ document.getElementById("list").addEventListener("click", function(e) {
     numberOfClick++;
 
     // add item to yearPanel
-    addItemToYearPanel(clickedElementText,clickedElementId);
+    addItemToYearPanels(clickedElementText,clickedElementId);
 
 })
 
 
-function addItemToYearPanel(itemText,itemId){
+function addItemToYearPanels(itemText,itemId){
     
     // create new item text field
     var newItem = document.createElement("INPUT");
     newItem.setAttribute("type", "text");
     newItem.setAttribute("class", itemId + numberOfClick);
 
-
     document.getElementsByClassName("yearPanel")[0].appendChild(newItem);
-
-    
-
 
     // append the newItem to all divS with yearPanel class
     for (i = 1; i < howManyYears; i++) {
@@ -41,5 +67,73 @@ function addItemToYearPanel(itemText,itemId){
         document.getElementsByClassName("yearPanel")[i].appendChild(cln);
       }
     
+    
+}
+
+
+function createTabsNPanels(year){
+
+
+while (yearSelected < currentYear){
+   
+   
+    createNewTab(yearSelected);
+   
+    createNewPAnel();
+   
+    yearSelected++;
+   
+}
+
+   // create panel for the tab
+
+
+//    var newPanel = document.createElement('div');
+//    newPanel.setAttribute('class','yearPanel');
+//    newPanel.setAttribute('class','tab-pane');
+//    newPanel.setAttribute('class','fade');
+//    newPanel.setAttribute('role','tabpanel');
+//    newPanel.setAttribute('aria-labelledby','contact-tab');
+//    newPanel.setAttribute('id','year2020');
+
+
+//    var myTabContent = document.getElementById('myTabContent');
+
+//    myTabContent.appendChild(newPanel);
+
+
+
+}
+
+
+function createNewTab(tabYear){
+
+
+        // create new tab
+        var newListItem = document.createElement('li');
+        newListItem.setAttribute('class','nav-item');
+
+        var newAnchor = document.createElement('a');
+        newAnchor.setAttribute('class','nav-link');
+        newAnchor.setAttribute('href','#year' + tabYear);
+        newAnchor.setAttribute('id','year' + tabYear +'-tab');
+        newAnchor.setAttribute('data-toggle','tab');
+        newAnchor.setAttribute('role','tab');
+        newAnchor.setAttribute('aria-controls','contact');
+        newAnchor.setAttribute('aria-selected','false');
+        newAnchor.appendChild(document.createTextNode(tabYear));
+
+
+
+
+        newListItem.appendChild(newAnchor);
+
+        var tabs = document.getElementById('myTab');
+        tabs.appendChild(newListItem);
+
+}
+
+
+function createNewPAnel(){
     
 }
